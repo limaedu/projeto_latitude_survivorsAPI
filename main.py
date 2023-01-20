f81ebb88d38e9611bd161caf0d0c7854e7d4a0da
limae <lima.edu@poli.ufrj.br>
2023-01-20 17:27:03 -0300

    added redirection

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from fastapi import FastAPI
 from survivors.routers import survivors_routers
+from fastapi.responses import RedirectResponse
 from exceptions import IdNotFound, id_not_found_exception_handler, CoordinatesOutOfRange, coordinates_out_of_range_exception_handler
 
+description = """
+SurvivosAPI is an API that helps you locate other survivors. 
 
-app = FastAPI()
+## Survivors
 
-@app.get("/")
+You will be able to:
+
+* **Get all survivors** 
+* **Create a survivor** 
+* **Get a survivor by id** 
+* **Update a survivor** 
+* **Report a survivor as infected** 
+* **Get the closest survivor from a survivor** 
+
+
+
+Developed by: **Eduardo Lima**
+"""
+app = FastAPI(title="SurvivorsAPI",
+    description=description,
+    version="0.0.1",
+    contact={
+        "email": "lima.edu.araujo@gmail.com",
+    },
+    )
+
+@app.get("/", include_in_schema=False)
 async def root():
-    return "[Latitude.sh] Maxihost - Backend test by Eduardo Lima"
+    return RedirectResponse(url = '/survivors')
     
 app.include_router(survivors_routers.router)
 app.add_exception_handler(IdNotFound, id_not_found_exception_handler)
